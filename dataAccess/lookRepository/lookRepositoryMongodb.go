267e4b1c28194ea31195a627884f3b6ea423c290
juansgt <32044146+juansgt@users.noreply.github.com>
2023-01-27 22:24:32 +0100

    Añadida la clase que de repositorio que conecta con Mongopara la colección de looks

---
+++ dataAccess/lookRepository/lookRepositoryMongodb.go
@@ -0,0 +1,30 @@
+package lookRepository
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type LookRepositoryMongodb struct {
+	looksCollection *mongo.Collection
+}
+
+func NewLookRepositoryMongodb(database mongo.Database) *LookRepositoryMongodb {
+	return &LookRepositoryMongodb{
+		looksCollection: database.Collection("looks"),
+	}
+}
+
+func (lookRepositoryMongodb *LookRepositoryMongodb) findLooks() []Look {
+	var looks []Look
+	
+	cur, err := lookRepositoryMongodb.looksCollection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		panic(err)
+	}
+	cur.Decode(looks)
+
+	return looks 
+}
