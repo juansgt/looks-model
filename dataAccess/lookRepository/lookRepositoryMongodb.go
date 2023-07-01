2eb14385da7f95ed36e7284c36a0c9bcef0a2b0a
juansgt <32044146+juansgt@users.noreply.github.com>
2023-07-02 01:16:48 +0200

    Ajuste para asignar el Id de Mongo al Objeto Look del dominio

--- dataAccess/lookRepository/lookRepositoryMongodb.go
+++ dataAccess/lookRepository/lookRepositoryMongodb.go
@@ -4,13 +4,14 @@ import (
 	"context"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type LookBson struct {
-	_id    string `bson:"_id,omitempty"`
-	Colour string `bson:"colour,omitempty"`
-	Brand  string `bson:"brand,omitempty"`
+	Id     primitive.ObjectID `bson:"_id,omitempty"`
+	Colour string             `bson:"colour,omitempty"`
+	Brand  string             `bson:"brand,omitempty"`
 }
 
 type LookRepositoryMongodb struct {
@@ -32,14 +33,12 @@ func (lookRepositoryMongodb *LookRepositoryMongodb) FindLooks() []Look {
 		panic(err)
 	}
 
-	// cursor.Decode(looksBson)
-
 	if err = cursor.All(context.TODO(), &looksBson); err != nil {
 		panic(err)
 	}
 
 	for _, lookBson := range looksBson {
-		looks = append(looks, *NewLook(lookBson._id, lookBson.Colour, lookBson.Brand))
+		looks = append(looks, *NewLook(lookBson.Id.String(), lookBson.Colour, lookBson.Brand))
 	}
 
 	return looks
